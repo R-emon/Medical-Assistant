279c2dd1c8c99daea6df42d89a9b3d63fe10a140
R-emon <rabiulislamemon1@gmail.com>
2020-12-14 00:08:22 +0600

    Added all layred panels for 3 different user

--- src/package1/Dashboard.java
+++ src/package1/Dashboard.java
@@ -2,36 +2,93 @@ package package1;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.Image;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import javax.swing.JLayeredPane;
+import java.awt.CardLayout;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
 
 public class Dashboard extends JFrame{
 
 	private JPanel dashboardContentPane;
+	private JPanel patientTopBarPanel;
+	private JPanel doctorTopBarPanel;
+	private JPanel nurseTopBarPanel;
+	private JPanel patientSideBarPanel;
+	private JPanel doctorSideBarPanel;
+	private JPanel nurseSideBarPanel;
+	private Image topBar= new ImageIcon(Dashboard.class.getResource("/images/dashboardTopBar.png")).getImage().getScaledInstance(1428, 127, Image.SCALE_SMOOTH);
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Dashboard window = new Dashboard();
-					window.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+//	public static void main(String[] args) {
+//		EventQueue.invokeLater(new Runnable() {
+//			public void run() {
+//				try {
+//					Dashboard window = new Dashboard();
+//					window.setVisible(true);
+//				} catch (Exception e) {
+//					e.printStackTrace();
+//				}
+//			}
+//		});
+//	}
 
 	/**
 	 * Create the application.
 	 */
 	public Dashboard() {
+		getContentPane().setBackground(new Color(229,234,230));
+		getContentPane().setLayout(null);
 		
-		setBounds(100, 100, 450, 300);
+		JLayeredPane topBarLayeredPane = new JLayeredPane();
+		topBarLayeredPane.setBounds(31, 25, 1428, 130);
+		getContentPane().add(topBarLayeredPane);
+		topBarLayeredPane.setLayout(new CardLayout(0, 0));
+		
+		patientTopBarPanel = new JPanel();
+		topBarLayeredPane.add(patientTopBarPanel, "name_38695801413200");
+		patientTopBarPanel.setLayout(null);
+		
+		JLabel topBarImageLabel = new JLabel("");
+		topBarImageLabel.setIcon(new ImageIcon(topBar));
+		//topBarImageLabel.setIcon(new ImageIcon(Dashboard.class.getResource("/images/dashboardTopBar.png")));
+		topBarImageLabel.setBackground(new Color(229,234,230));
+		topBarImageLabel.setForeground(new Color(229,234,230));
+		topBarImageLabel.setBounds(0, 0, 1428, 130);
+		patientTopBarPanel.add(topBarImageLabel);
+		
+		doctorTopBarPanel = new JPanel();
+		topBarLayeredPane.add(doctorTopBarPanel, "name_38730535960000");
+		doctorTopBarPanel.setLayout(null);
+		
+		nurseTopBarPanel = new JPanel();
+		topBarLayeredPane.add(nurseTopBarPanel, "name_38755163090900");
+		nurseTopBarPanel.setLayout(null);
+		
+		JLayeredPane sideBarLayeredPane = new JLayeredPane();
+		sideBarLayeredPane.setBounds(31, 198, 338, 536);
+		getContentPane().add(sideBarLayeredPane);
+		sideBarLayeredPane.setLayout(new CardLayout(0, 0));
+		
+		patientSideBarPanel = new JPanel();
+		sideBarLayeredPane.add(patientSideBarPanel, "name_38915446570600");
+		patientSideBarPanel.setLayout(null);
+		
+		doctorSideBarPanel = new JPanel();
+		sideBarLayeredPane.add(doctorSideBarPanel, "name_38948241781600");
+		doctorSideBarPanel.setLayout(null);
+		
+		nurseSideBarPanel = new JPanel();
+		sideBarLayeredPane.add(nurseSideBarPanel, "name_38965842404800");
+		nurseSideBarPanel.setLayout(null);
+		
+		setBounds(10, 10, 1500, 800);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		initialize();
 	}
@@ -44,5 +101,4 @@ public class Dashboard extends JFrame{
 		dashboardContentPane.setBorder(new EmptyBorder(5,5,5,5));
 		dashboardContentPane.setLayout(new BorderLayout(0,0));
 	}
-
 }
